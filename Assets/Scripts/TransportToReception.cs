22e4fb172e892717f89ff68fb9653bb7c0c05d87
Calvin Feng <calvinf@datacom.co.nz>
2021-02-03 15:44:20 +1300

    init

---
+++ Assets/Scripts/TransportToReception.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TransportToReception : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        if (hit.gameObject.tag == "Player")
+        {
+            Scene sceneToLoad = SceneManager.GetSceneByName("Reception");
+            SceneManager.LoadScene(sceneToLoad.name, LoadSceneMode.Additive);
+            SceneManager.MoveGameObjectToScene(hit.gameObject, sceneToLoad);
+        }
+    }
+}
---
+++ Assets/Scripts/TransportToTreatment.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TransportToTreatment : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        if (hit.gameObject.tag == "Player")
+        {
+            Scene sceneToLoad = SceneManager.GetSceneByName("TreatmentRoom");
+            SceneManager.LoadScene(sceneToLoad.name, LoadSceneMode.Additive);
+            SceneManager.MoveGameObjectToScene(hit.gameObject, sceneToLoad);
+        }
+    }
+}
