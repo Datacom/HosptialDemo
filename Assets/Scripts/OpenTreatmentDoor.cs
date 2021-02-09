daf3d34b0db7740430aa39a29f2468e11254f713
Jonathan Yan <jono2496@gmail.com>
2021-02-09 16:17:38 +1300

    Add sound effect to the doors
    Add sound effect to the doors

--- Assets/Scripts/OpenTreatmentDoor.cs
+++ Assets/Scripts/OpenTreatmentDoor.cs
@@ -5,6 +5,7 @@ using Microsoft.MixedReality.Toolkit.Input;
 
 public class OpenTreatmentDoor : MonoBehaviour, IMixedRealityInputHandler
 {
+    bool isDoorOpen;
     [SerializeField]
     private Animator animator;
     public void OnInputDown(InputEventData eventData)
@@ -13,12 +14,16 @@ public class OpenTreatmentDoor : MonoBehaviour, IMixedRealityInputHandler
 
     public void OnInputUp(InputEventData eventData)
     {
+        if (!isDoorOpen)
+            transform.GetComponent<AudioSource>().Play();
         animator.SetBool("IsOpening", true);
+        isDoorOpen = true;
     }
 
     // Start is called before the first frame update
     void Start()
     {
+        isDoorOpen = false;
         animator = transform.GetComponent<Animator>();
     }
 
